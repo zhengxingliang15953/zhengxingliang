55a71dec1407dc7b40404245314abf52e8f34c33
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-02-11 17:15:24 +0800

    zxl

---
+++ firstUni/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<button type="primary">{{title}}</button>
+		<view hover-class="hoverBox">
+			view
+		</view>
+		<scroll-view scroll-y="true"><!--可滚动view-->
+			fd
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '按钮',
+			}
+		},
+	}
+</script>
+
+<style scoped lang="less">
+	.hoverBox{
+		background-color:skyblue;
+	}
+</style>
