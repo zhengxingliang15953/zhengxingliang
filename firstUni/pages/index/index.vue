5901788259c114b79e2762032f4578567e76ed88
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-02-12 09:48:53 +0800

    zxl

--- firstUni/pages/index/index.vue
+++ firstUni/pages/index/index.vue
@@ -1,11 +1,11 @@
 <template>
 	<view>
 		<button type="primary">{{title}}</button>
-		<view hover-class="hoverBox">
-			view
+		<view hover-class="hoverBox"><!--正常view-->
+			正常的view
 		</view>
 		<scroll-view scroll-y="true"><!--可滚动view-->
-			fd
+			可滚动view
 		</scroll-view>
 	</view>
 </template>
