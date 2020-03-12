ddb7c29139d4f2262603f1d252d2aa69cdddecb5
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-03-12 20:31:36 +0800

    zxl

--- webpack/src/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * 开发环境打包 webpack ./src/main.js -o ./build/index.js --mode=development
- * production生产环境
- */
-console.log("a");
-import '../assets/file.css';
-import '../assets/file.less';
-console.log("abc");
