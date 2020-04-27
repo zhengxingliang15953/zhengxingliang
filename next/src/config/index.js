9885957e69d5936305639211700672a2c3b6ea5e
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-04-27 08:48:50 +0800

    zxl

--- next/src/config/index.js
+++
@@ -1,8 +0,0 @@
-import development from "./development";
-import production from "./production";
-
-export const isDev = process.env.NODE_ENV === "development";
-
-const config = isDev ? development : production;
-
-export default config;
