52b0dfd22822a48ecc2ed9a814d5e1417269b25a
郑兴樑 <mac@macbookdeMacBook-Pro-2.local>
2020-04-30 08:35:45 +0800

    zxl

--- libraryBack/src/views2/index.vue
+++ libraryBack/src/views2/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="index">
-    <Layout>
+    <Layout style="height:100%;">
       <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
         <Menu :active-name="select" @on-select="menuSelect" theme="dark" width="auto" :class="menuitemClasses">
           <MenuItem name="1" to="/index/book">
@@ -48,7 +48,7 @@
           ></Icon>
           <Button type="primary" style="margin-right:70%;" @click="back">退出</Button><span style="font-size:0.08rem;color:#1E90FF;">{{value1}}</span>
         </Header>
-        <Content id="content" :style="{margin: '20px', background: '#fff',minHeight:`622px`}">
+        <Content id="content" :style="{margin: '20px', background: '#fff'}">
             <router-view></router-view>
         </Content>
       </Layout>
@@ -115,6 +115,7 @@ export default {
   position: relative;
   border-radius: 4px;
   overflow: hidden;
+  height: 100%;
 }
 
 .layout-header-bar {
